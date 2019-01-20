665425ca7096dfdaf7c3306dfe616ba74c33c7fa
Vlad <s304@rambler.ru>
2019-01-20 20:15:17 +0200

    added menu.

--- app/src/main/java/com/example/tsego/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tsego/myapplication/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.tsego.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.info_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
